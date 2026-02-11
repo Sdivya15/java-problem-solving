ed41c268cddcb82ce40c328a0799c16a986f2049
DivyaS <127476291+Sdivya15@users.noreply.github.com>
2026-02-11 06:47:23 +0530

    Added MenuPage Of ATM simulation

---
+++ src/main/java/org/example/atm/pinsimulation/MenuPage.java
@@ -0,0 +1,13 @@
+package org.example.atm.pinsimulation;
+
+public class MenuPage {
+
+    public static void showMenu(){
+        System.out.println("Welcome to ATM Menu!");
+        System.out.println("Select an Option");
+        System.out.println("1.Check Balance");
+        System.out.println("2.Deposit");
+        System.out.println("3.Withdraw");
+        System.out.println("4.Exit");
+    }
+}
