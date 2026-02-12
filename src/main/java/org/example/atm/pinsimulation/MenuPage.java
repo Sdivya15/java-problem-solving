5ccf32dd0f8927f1b12d3b949c9b3daa18c5973d
DivyaS <127476291+Sdivya15@users.noreply.github.com>
2026-02-12 07:01:42 +0530

    Added MenuPage Of ATM simulation

--- src/main/java/org/example/atm/pinsimulation/MenuPage.java
+++ src/main/java/org/example/atm/pinsimulation/MenuPage.java
@@ -1,13 +1,65 @@
 package org.example.atm.pinsimulation;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class MenuPage {
 
-    public static void showMenu(){
-        System.out.println("Welcome to ATM Menu!");
-        System.out.println("Select an Option");
-        System.out.println("1.Check Balance");
-        System.out.println("2.Deposit");
-        System.out.println("3.Withdraw");
-        System.out.println("4.Exit");
+
+    public static void showMenu(Account person,Scanner sc) {
+
+
+        while (true) {
+            System.out.println("Welcome to ATM Menu");
+            System.out.println("Select an option");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Deposit");
+            System.out.println("4.Exit");
+
+
+            if (!sc.hasNextInt()) {
+                System.out.println("Invalid input");
+                sc.next();
+                continue;
+            }
+
+            int choice = sc.nextInt();
+
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Current Balance: " + person.getBalance());
+                    break;
+
+
+                case 2:
+                    System.out.println("Enter withdrawal amount");
+                    BigDecimal withdraw = new BigDecimal(sc.next());
+                    if (person.withdraw(withdraw)) {
+                        System.out.println("Please collect your cash");
+                    } else {
+                        System.out.println("Insufficient Balance");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Enter deposit amount");
+                    BigDecimal deposit = new BigDecimal(sc.next());
+                    person.deposit(deposit);
+                    System.out.println("Amount deposited successfully");
+                    break;
+
+
+                case 4:
+                    System.out.println("Thank you");
+                    return;
+
+
+                default:
+                    System.out.println("Invalid choice");
+            }
+
+
+        }
     }
 }
